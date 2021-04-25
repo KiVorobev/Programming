97e090d1cc56d89de782c95039ee32a106101891
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-04-25 21:07:56 +0300

    Create MeleeWeapon.java

---
+++ Lab5/src/data/MeleeWeapon.java
@@ -0,0 +1,14 @@
+package data;
+
+/**
+ * @author Kirill Vorobyev
+ * @version 1.1
+ * Enum class for describing melee weapon of element
+ */
+
+public enum MeleeWeapon {
+    CHAIN_SWORD,
+    CHAIN_AXE,
+    POWER_BLADE,
+    POWER_FIST;
+}
