6c10ad819850a8eefc8b3a419fe0877d587ed3cf
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-06 13:56:38 +0300

    Update Commander.java

--- Lab5/src/Commander.java
+++ Lab5/src/Commander.java
@@ -8,11 +8,17 @@ import java.util.Scanner;
  */
 public class Commander {
 
-    /** Field for receiving user`s command */
+    /**
+     * Field for receiving user`s command
+     */
     private String userCommand;
-    /** Field for separating user input into a command and an argument to it */
+    /**
+     * Field for separating user input into a command and an argument to it
+     */
     private String[] cleanUserCommand;
-    /** Collection manager for realising user`s commands */
+    /**
+     * Collection manager for realising user`s commands
+     */
     private CollectionManagement manager;
 
     {
@@ -21,13 +27,16 @@ public class Commander {
 
     /**
      * Constructor for making a commander
+     *
      * @param manager - CollectionManager class object which will realise user`s commands
      */
-    public Commander (CollectionManagement manager){
+    public Commander(CollectionManagement manager) {
         this.manager = manager;
     }
 
-    /** Method for starting console mod */
+    /**
+     * Method for starting console mod
+     */
     public void consoleMod() {
         try {
             Scanner commandReader = new Scanner(System.in);
@@ -39,65 +48,69 @@ public class Commander {
                 //Replacing the pattern with single space
                 String notCleanUserCommand = userCommand.replaceAll(regex, " ");
                 cleanUserCommand = notCleanUserCommand.trim().split(" ", 2);
-                switch (cleanUserCommand[0]) {
-                    case "":
-                        break;
-                    case "help":
+                try {
+                    switch (cleanUserCommand[0]) {
+                        case "":
+                            break;
+                        case "help":
                             manager.help();
-                        break;
-                    case "info":
+                            break;
+                        case "info":
                             manager.info();
-                        break;
-                    case "show":
+                            break;
+                        case "show":
                             manager.show();
-                        break;
-                    case "insert":
+                            break;
+                        case "insert":
                             manager.insert(cleanUserCommand[1]);
-                        break;
-                    case "update":
+                            break;
+                        case "update":
                             manager.update(cleanUserCommand[1]);
-                        break;
-                    case "remove_key":
+                            break;
+                        case "remove_key":
                             manager.removeKey(cleanUserCommand[1]);
-                        break;
-                    case "clear":
+                            break;
+                        case "clear":
                             manager.clear();
-                        break;
-                    case "save":
+                            break;
+                        case "save":
                             manager.save();
-                        break;
-                    case "execute_script":
+                            break;
+                        case "execute_script":
                             manager.executeScript(cleanUserCommand[1]);
-                        break;
-                    case "exit":
+                            break;
+                        case "exit":
                             manager.exit();
-                        break;
-                    case "remove_greater":
+                            break;
+                        case "remove_greater":
                             System.out.println("Enter characteristics of element, which will be compared with elements in collection.");
                             manager.removeGreater(manager.scanHealth());
-                        break;
-                    case "replace_if_greater":
+                            break;
+                        case "replace_if_greater":
                             manager.replaceIfGreater(cleanUserCommand[1]);
-                        break;
-                    case "remove_greater_key":
+                            break;
+                        case "remove_greater_key":
                             manager.removeGreaterKey(cleanUserCommand[1]);
-                        break;
-                    case "group_counting_by_coordinates":
+                            break;
+                        case "group_counting_by_coordinates":
                             manager.groupCountingByCoordinates();
-                        break;
-                    case "filter_by_chapter":
+                            break;
+                        case "filter_by_chapter":
                             manager.filterByChapter(manager.scanChapter());
-                        break;
-                    case "filter_starts_with_name":
+                            break;
+                        case "filter_starts_with_name":
                             manager.filterStartsWithName(cleanUserCommand[1]);
-                        break;
-                    default:
-                        System.out.println("Unknown command. Write 'help' for reference.");
+                            break;
+                        default:
+                            System.out.println("Unknown command. Write 'help' for reference.");
+                    }
+                } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+                    System.out.println("This command needs an argument.");
                 }
             }
         } catch (NoSuchElementException noSuchElementException) {
-            System.out.println("You clicked shortcut for finishing a program.");
-            System.exit(0);
+                    System.out.println("You clicked shortcut for finishing a program.");
+                    System.exit(0);
         }
     }
 }
