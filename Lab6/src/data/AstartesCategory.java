7c36020bf91e69f4cb508a36f87441584c513338
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-07 22:17:29 +0300

    Create AstartesCategory.java

---
+++ Lab6/src/data/AstartesCategory.java
@@ -0,0 +1,12 @@
+package data;
+
+/**
+ * @author Kirill Vorobyev
+ * @version 1.0
+ * Enum class for describing category of element
+ */
+public enum AstartesCategory {
+    ASSAULT,
+    TACTICAL,
+    CHAPLAIN;
+}
