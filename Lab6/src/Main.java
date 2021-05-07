7b0937dcc3d7d840d9a00e8a9a78a8e43e7fe42c
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-07 22:16:59 +0300

    Create Main.java

---
+++ Lab6/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+public class Main {
+    /**
+     * Main class for starting a program
+     * @author Kirill Vorobyev
+     * @version 1.0
+     * @param args - args for program successfully working
+     */
+    public static void main(String[] args) throws IOException {
+        try {
+            Commander commander = new Commander(new CollectionManagement(args[0]));
+            commander.consoleMod();
+        } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            System.out.println("Don`t forget to enter a correct path to file next time.");
+            System.exit(1);
+        }
+    }
+}
