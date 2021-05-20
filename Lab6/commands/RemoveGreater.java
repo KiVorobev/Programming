50cfb7df35852bf6860dd8c20101b6d6c7c78683
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-20 15:12:48 +0300

    Create RemoveGreater.java

---
+++ Lab6/commands/RemoveGreater.java
@@ -0,0 +1,59 @@
+package commands;
+
+import data.Manager;
+import data.SpaceMarine;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class RemoveGreater extends Command {
+
+    /**
+     * Method for removing elements from collection if it`s health more than entered health
+     */
+    public String action(String in, Manager collection) {
+        String message = null;
+        try {
+            String test = in;
+            while (test.substring(0, 1).equals(" ")) {
+                test = test.substring(1, test.length());
+            }
+            int health;
+            if (test.indexOf(" ") > 0) {
+                health = Integer.parseInt(test.substring(0, test.indexOf(" ")));
+            } else {
+                health = Integer.parseInt(test);
+            }
+            int counter = 0;
+            boolean check = false;
+            Set<Integer> keys = new HashSet<>();
+            for (Map.Entry<Integer, SpaceMarine> entry : collection.getSpaceMarines().entrySet()) {
+                SpaceMarine test2 = entry.getValue();
+                if (health < test2.getHealth()) {
+                    keys.add(entry.getKey());
+                    check = true;
+                }
+            }
+            if (check) {
+                for (Integer last : keys) {
+                    counter += 1;
+                    collection.getSpaceMarines().remove(last);
+                }
+            }
+            if (counter == 0) {
+                message = "There are no elements in the collection that exceed the specified one.";
+            }
+            if (counter == 1) {
+                message = "1 element removed successfully.";
+            }
+            if (counter > 1) {
+                message = counter + " elements removed successfully.";
+            }
+        } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+            message = "As an argument you need to enter a number.";
+        }
+        collection.save();
+        return message;
+    }
+}
