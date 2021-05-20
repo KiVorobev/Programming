21e5c9ca703ee15c2b1c876551c9eeaa5fd267aa
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-20 15:13:26 +0300

    Create SpaceMarines.java

---
+++ Lab6/data/SpaceMarines.java
@@ -0,0 +1,41 @@
+package data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.*;
+
+/**
+ * @author Kirill Vorobyev
+ * @version 1.2
+ * Class for describing a collection in a marshaling-ready view
+ */
+
+@XmlRootElement (name = "spaceMarines")
+@XmlAccessorType (XmlAccessType.NONE)
+public class SpaceMarines {
+
+    /**
+     * Field persons - map for keeping collection
+     */
+    @XmlElement (name = "spaceMarine")
+    private Map<Integer, SpaceMarine> spaceMarine = new HashMap<>();
+
+    /**
+     * Method for get spaceMarines map
+     *
+     * @return Map<Integer, SpaceMarine> spaceMarines
+     */
+    public Map<Integer, SpaceMarine> getSpaceMarines() {
+        return spaceMarine;
+    }
+
+    /**
+     * Method for passing a value to the spaceMarines hashmap
+     */
+    public void setSpaceMarines(Map<Integer, SpaceMarine> spaceMarines) {
+        this.spaceMarine = spaceMarines;
+    }
+}
+
