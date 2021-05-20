08203cb851cc51cec8102522fe23947a675da751
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-20 16:47:18 +0300

    Update FilterByChapter.java

--- Lab6/commands/FilterByChapter.java
+++ Lab6/commands/FilterByChapter.java
@@ -3,9 +3,20 @@ package commands;
 import data.Manager;
 import data.SpaceMarine;
 
+/**
+ * Class of command 'filter_by_chapter'
+ * @author Kirill Vorobyev
+ * @version 1.0
+ */
 public class FilterByChapter extends Command {
 
-    /** Method for printing elements whose chapter field value is equal to it's value of chapter */
+    /**
+     * Method for executing this command
+     *
+     * @param chapterName - name of chapter
+     * @param collection - collection
+     * @return - String description of command
+     */
     public String action(String chapterName, Manager collection) {
             String test = chapterName.trim();
             StringBuilder message = new StringBuilder();
