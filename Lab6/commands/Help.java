47ba21edf4932eaecc0f2a613f80a89d472fe482
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-20 16:47:28 +0300

    Update Help.java

--- Lab6/commands/Help.java
+++ Lab6/commands/Help.java
@@ -4,8 +4,19 @@ import data.Manager;
 
 import java.util.*;
 
+/**
+ * Class of command 'help'
+ * @author Kirill Vorobyev
+ * @version 1.0
+ */
 public class Help extends Command {
 
+    /**
+     * Method for executing this command
+     *
+     * @param collection - collection
+     * @return - String description of command
+     */
     public String action(Manager collection){
         StringBuilder message = new StringBuilder();
         for (Map.Entry<String, String> entry : collection.getInfoCommands().entrySet()) {
