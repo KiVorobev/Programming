0948ef736e3f3b5baf1cff04683735c8e34036da
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-20 16:48:37 +0300

    Update UDPServer.java

--- Lab6/UDPServer.java
+++ Lab6/UDPServer.java
@@ -5,48 +5,34 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 
+/**
+ * Class for starting a server
+ * @author Kirill Vorobyev
+ * @version 1.0
+ */
 public class UDPServer {
     /** Server UDP socket running on this port */
     public final static int servicePort = 2828;
     public static int port;
     /** Socket for communication */
     private static DatagramSocket socket;
+    /** Field for storing client commands */
     private static String[] userCommand;
+    /** Map for printing available commands for user */
     private static HashMap<String, Command> commands;
+    /** Field for storing the IP-address */
     private static InetAddress address;
+    /** Array of bytes for organizing a packet for receiving data from client */
     static byte[] buffer = new byte[65535];
+    /** Array of bytes for organizing a packet for sending answer to client */
     static byte[] secondBuffer = new byte[65535];
 
-
-
-    UDPServer() {
-        try {
-            socket = new DatagramSocket(servicePort);
-        } catch (SocketException socketException) {
-            socketException.printStackTrace();
-        }
-        commands = new HashMap<>();
-        commands.put("help", new Help());
-        commands.put("info", new Info());
-        commands.put("show", new Show());
-        commands.put("insert number_of_key", new Insert());
-        commands.put("update id", new Update());
-        commands.put("remove_key number_of_key", new RemoveKey());
-        commands.put("clear", new Clear());
-        commands.put("execute_script file_name", new ExecuteScript());
-        commands.put("exit", new Exit());
-        commands.put("remove_greater value_of_health", new RemoveGreater());
-        commands.put("replace_if_greater value_of_health", new ReplaceIfGreater());
-        commands.put("remove_greater_key number_of_key", new RemoveGreaterKey());
-        commands.put("group_counting_by_coordinates", new GroupCountingByCoordinates());
-        commands.put("filter_by_chapter chapter_name", new FilterByChapter());
-        commands.put("filter_starts_with_name name", new FilterStartsWithName());
-    }
-
+    /**
+     * Server entry point
+     */
     public static void main(String[] args) throws IOException, InterruptedException {
         try {
             Manager manager = new Manager(args[0]);
@@ -65,10 +51,10 @@ public class UDPServer {
     }
 
     /**
-     * Модуль чтения запроса
+     * Module for receiving messages from the client
      *
-     * @return - возвращает десериализованную команду
-     * @throws IOException - ошибка чтения запроса
+     * @return - String client command
+     * @throws IOException - receiving exception
      */
     public static String read() throws IOException {
         DatagramPacket fromClientPacket = new DatagramPacket(buffer, buffer.length);
@@ -82,10 +68,10 @@ public class UDPServer {
     }
 
     /**
-     * Модуль отправки ответов клиенту
+     * Module for sending answers to the client
      *
-     * @throws IOException - ошибка чтения запроса
-     * @throws InterruptedException - ошибка ожидания
+     * @throws IOException - receiving exception
+     * @throws InterruptedException - wait exception
      */
     public static void write(Manager collection) throws IOException, InterruptedException {
         String answer;
@@ -99,7 +85,11 @@ public class UDPServer {
         socket.send(toClientPacket);
     }
 
-    /** Модуль выполнения команд */
+    /**
+     * Module for execution of commands without argument
+     *
+     * @return - String description of command
+     */
     public static String easyExecution(String firstArg, Manager manager){
         String answer;
         switch (firstArg) {
@@ -149,7 +139,11 @@ public class UDPServer {
         return answer;
     }
 
-    /** Модуль выполнения команд */
+    /**
+     * Module for execution of commands with argument
+     *
+     * @return - String description of command
+     */
     public static String hardExecution(String firstArg, String secondArg, Manager manager){
         String answer;
         switch (firstArg) {
