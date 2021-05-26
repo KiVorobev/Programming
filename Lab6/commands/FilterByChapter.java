1d17448aa51059ca258ff89b49b58c39e6a4d339
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-26 20:06:17 +0300

    Update FilterByChapter.java

--- Lab6/commands/FilterByChapter.java
+++ Lab6/commands/FilterByChapter.java
@@ -13,23 +13,23 @@ public class FilterByChapter extends Command {
     /**
      * Method for executing this command
      *
-     * @param chapterName - name of chapter
-     * @param collection - collection
-     * @return - String description of command
+     * @param chapterName name of chapter
+     * @param collection collection
+     * @return String description of command
      */
     public String action(String chapterName, Manager collection) {
-            String test = chapterName.trim();
-            StringBuilder message = new StringBuilder();
-            boolean check = false;
-            for (SpaceMarine spaceMarine : collection.getSpaceMarines().values()) {
-                if (spaceMarine.getChapter().getChapterName().equals(test)) {
-                    message.append(spaceMarine.toString());
-                    check = true;
-                }
+        String test = chapterName.trim();
+        StringBuilder message = new StringBuilder();
+        boolean check = false;
+        for (SpaceMarine spaceMarine : collection.getSpaceMarines().values()) {
+            if (spaceMarine.getChapter().getChapterName().equals(test)) {
+                message.append(spaceMarine.toString());
+                check = true;
             }
-            if (!check) {
-                message.append("There are no items in the collection with this value.");
-            }
-            return message.toString();
         }
+        if (!check) {
+            message.append("There are no items in the collection with this value.");
+        }
+        return message.toString();
     }
+}
