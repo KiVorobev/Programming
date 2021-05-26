82b98168038084ca9e7c7995c44b79e2290b0fb3
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-26 20:06:13 +0300

    Update Exit.java

--- Lab6/commands/Exit.java
+++ Lab6/commands/Exit.java
@@ -12,11 +12,11 @@ public class Exit extends Command {
     /**
      * Method for executing this command
      *
-     * @param collection - collection
-     * @return - String phrase for close client
+     * @param collection collection
+     * @return String phrase for close client
      */
     public String action(Manager collection){
         collection.save();
-        return "close client";
+        return "Program will be finished now. See you again:)";
     }
 }
