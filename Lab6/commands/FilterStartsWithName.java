3d4eab8169a9c86e28a1478ec0e7e4ab5c1e2ca0
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-05-31 18:38:33 +0300

    Update FilterStartsWithName.java

--- Lab6/commands/FilterStartsWithName.java
+++ Lab6/commands/FilterStartsWithName.java
@@ -1,8 +1,11 @@
 package commands;
 
 
-import data.Manager;
+import data.FileWorker;
 import data.SpaceMarine;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Class of command 'filter_starts_with_name'
@@ -18,7 +21,7 @@ public class FilterStartsWithName extends Command {
      * @param collection collection
      * @return String description of command
      */
-    public String action(String startName, Manager collection) {
+    public String action(String startName, FileWorker collection) {
         try {
             String test = startName;
             while (test.substring(0, 1).equals(" ")) {
@@ -26,13 +29,25 @@ public class FilterStartsWithName extends Command {
             }
             StringBuilder message = new StringBuilder();
             boolean check = false;
-            for (SpaceMarine spaceMarine : collection.getSpaceMarines().values()) {
-                if (spaceMarine.getName().substring(0, test.length()).equals(test)) {
-                    message.append(spaceMarine);
-                    check = true;
+            ArrayList<Integer> keys = new ArrayList<>();
+            for (Map.Entry<Integer, SpaceMarine> entry : collection.getSpaceMarines().entrySet()) {
+                SpaceMarine exMarine = entry.getValue();
+                if (exMarine.getName().length() >= test.length()) {
+                    if (exMarine.getName().substring(0, test.length()).equals(test)) {
+                        keys.add(entry.getKey());
+                        check = true;
+                    }
                 }
             }
-            if (!check) {
+            if (check) {
+                for (int i = 0; i < keys.size(); i++) {
+                    for (Map.Entry<Integer, SpaceMarine> entry : collection.getSpaceMarines().entrySet()) {
+                        if (keys.get(i) == entry.getKey()) {
+                            message.append(entry.getKey() + " " + entry.getValue());
+                        }
+                    }
+                }
+            } else {
                 message.append("There are no items in the collection whose name begins with the given substring.");
             }
             return message.toString();
