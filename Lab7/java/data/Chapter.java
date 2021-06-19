41abf3ac43cefd9c52891556f28f379c8020654d
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-19 22:48:49 +0300

    Create Chapter.java

---
+++ Lab7/java/data/Chapter.java
@@ -0,0 +1,50 @@
+package data;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * @author Kirill Vorobyev
+ * @version 1.0
+ * Class for describing chapter of element
+ */
+@Embeddable
+public class Chapter {
+    /** field name */
+    @Column(nullable=false)
+    private String chapterName; //Field cannot be null, the string cannot be empty
+    /** field world */
+    @Column(nullable=false)
+    private String chapterWorld; //Field cannot be null
+
+    /** Constructor */
+    public Chapter(String name, String world) {
+        this.chapterName = name;
+        this.chapterWorld = world;
+    }
+
+    /** Default constructor */
+    public Chapter() {}
+
+    public String getChapterName() {
+        return chapterName;
+    }
+
+    public void setName(String name) {
+        this.chapterName = name;
+    }
+
+    public String getChapterWorld() {
+        return chapterWorld;
+    }
+
+    public void setWorld(String world) {
+        this.chapterWorld = world;
+    }
+
+    /** Method for printing this field into a string representation */
+    @Override
+    public String toString(){
+        return " { name = " + chapterName + ", world = " + chapterWorld + " }";
+    }
+}
