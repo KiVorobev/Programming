5a9012cf81fb0fa5908f1df6022860cc7c8fd8db
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-19 22:49:19 +0300

    Create User.java

---
+++ Lab7/java/database/User.java
@@ -0,0 +1,61 @@
+package database;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table (name = "users")
+public class User {
+    private String login;
+    private String password;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SpaceMarines> spaceMarines;
+    public User() {
+    }
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+        spaceMarines = new ArrayList<>();
+    }
+
+    public void removeSpaceMarine(SpaceMarines spaceMarine) {
+        spaceMarines.remove(spaceMarine);
+    }
+
+    public List<SpaceMarines> getSpaceMarines() {
+        return spaceMarines;
+    }
+
+    public void setSpaceMarine(List<SpaceMarines> spaceMarines) {
+        this.spaceMarines = spaceMarines;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setSpaceMarines(List<SpaceMarines> spaceMarines) {
+        this.spaceMarines = spaceMarines;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " + login;
+    }
+}
