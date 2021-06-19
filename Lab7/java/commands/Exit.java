7672339a9c7c36e2d8d3a51badb309fb51bbe515
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-19 22:48:08 +0300

    Create Exit.java

---
+++ Lab7/java/commands/Exit.java
@@ -0,0 +1,23 @@
+package commands;
+
+import data.*;
+import java.util.TreeMap;
+
+/**
+ * Class of command 'exit'
+ * @author Kirill Vorobyev
+ * @version 1.0
+ */
+public class Exit extends Command {
+
+    /**
+     * Method for executing this command
+     *
+     * @param collection collection
+     * @return String phrase for close client
+     */
+    public String action(TreeMap<Integer,SpaceMarine> collection){
+        collection.clear();
+        return "Program will be finished now. See you again:)";
+    }
+}
