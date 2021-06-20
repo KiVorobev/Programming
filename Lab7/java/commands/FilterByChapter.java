81258efc3e066768e205366a73b0016d3a34564d
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-20 18:50:59 +0300

    Update FilterByChapter.java

--- Lab7/java/commands/FilterByChapter.java
+++ Lab7/java/commands/FilterByChapter.java
@@ -20,29 +20,34 @@ public class FilterByChapter extends Command {
      * @param collection collection
      * @return String description of command
      */
-    public String action(String chapterName, TreeMap<Integer,SpaceMarine> collection) {
+    public String action(String chapterName, TreeMap<Integer,SpaceMarine> collection, String login) {
         String test = chapterName.trim();
         StringBuilder message = new StringBuilder();
         boolean check = false;
+        boolean notEmpty = false;
         ArrayList<Integer> keys = new ArrayList<>();
         for (Map.Entry<Integer, SpaceMarine> entry : collection.entrySet()) {
-            SpaceMarine exMarine = entry.getValue();
-            if (exMarine.getChapter().getChapterName().equals(test)) {
-                keys.add(entry.getKey());
-                check = true;
-            }
-        }
-        if (check) {
-            for (int i = 0; i < keys.size(); i++) {
-                for (Map.Entry<Integer, SpaceMarine> entry : collection.entrySet()) {
-                    if (keys.get(i) == entry.getKey()) {
-                        message.append(entry.getKey() + " " + entry.getValue());
-                    }
+            if (entry.getValue().getUser().equals(login)) {
+                notEmpty = true;
+                if (entry.getValue().getChapter().getChapterName().equals(test)) {
+                    keys.add(entry.getKey());
+                    check = true;
                 }
             }
-        } else {
-            message.append("There are no items in the collection with this value.");
         }
+        if (notEmpty) {
+            if (check) {
+                for (int i = 0; i < keys.size(); i++) {
+                    for (Map.Entry<Integer, SpaceMarine> entry : collection.entrySet()) {
+                        if (keys.get(i) == entry.getKey()) {
+                            message.append(entry.getKey() + " " + entry.getValue());
+                        }
+                    }
+                }
+            } else {
+                message.append("There are no items in the collection with this value.");
+            }
+        } else message.append("Your collection is empty.");
         return message.toString();
     }
 }
