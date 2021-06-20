40977a19a21a27ef5cf8c6befeb093b038ded444
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-20 18:51:21 +0300

    Update Insert.java

--- Lab7/java/commands/Insert.java
+++ Lab7/java/commands/Insert.java
@@ -1,8 +1,9 @@
 package commands;
 
 import data.*;
+import database.SpaceMarines;
+import database.SpaceMarinesDao;
 
-import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -21,12 +22,13 @@ public class Insert extends Command {
      * @return String description of command
      */
     public String action(String element, TreeMap<Integer,SpaceMarine> collection) {
+        SpaceMarinesDao spaceMarinesDao = new SpaceMarinesDao();
         String[] newElement = element.trim().split("\n", 12);
+        if (!collection.containsKey(Integer.parseInt(newElement[0]))) {
         Coordinates newCord = new Coordinates(Integer.parseInt(newElement[2]), Integer.parseInt(newElement[3]));
         AstartesCategory newCat = null;
         Weapon newWeapon = null;
         MeleeWeapon newMelee = null;
-        LocalDateTime date = LocalDateTime.parse(newElement[4]);
         if (!newElement[6].equals("null")) {
             newCat = AstartesCategory.valueOf(newElement[6]);
         }
@@ -37,13 +39,15 @@ public class Insert extends Command {
             newMelee = MeleeWeapon.valueOf(newElement[8]);
         }
         Chapter newChap = new Chapter(newElement[9], newElement[10]);
-        SpaceMarine finishSpaceMarine = new SpaceMarine(makeId(collection), newElement[1], newCord, String.valueOf(date),
-                Integer.parseInt(newElement[5]), newCat, newWeapon, newMelee, newChap, newElement[11]);
-        if (!collection.containsKey(Integer.parseInt(newElement[0]))) {
             int key = Integer.parseInt(newElement[0]);
-            finishSpaceMarine.setId(makeId(collection));
-            collection.put(key, finishSpaceMarine);
 
+        SpaceMarines finishSpaceMarines = new SpaceMarines(key,makeId(collection),
+                newElement[1], Integer.parseInt(newElement[2]), Integer.parseInt(newElement[3]), newElement[4], Integer.parseInt(newElement[5]), newCat,
+                newWeapon, newMelee, newElement[9], newElement[10], newElement[11]);
+        spaceMarinesDao.save(finishSpaceMarines);
+        SpaceMarine finishSpaceMarine = new SpaceMarine(makeId(collection), newElement[1], newCord, newElement[4],
+                Integer.parseInt(newElement[5]), newCat, newWeapon, newMelee, newChap, newElement[11]);
+            collection.put(key, finishSpaceMarine);
             return "Element added successfully.";
         } else {
             return "Such key already exists. Try again.";
