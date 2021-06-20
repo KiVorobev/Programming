c138dcf3d7a5e77e16addcf308d702c9c9d1ff37
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-20 18:50:47 +0300

    Update Clear.java

--- Lab7/java/commands/Clear.java
+++ Lab7/java/commands/Clear.java
@@ -1,6 +1,12 @@
 package commands;
 
 import data.SpaceMarine;
+import database.SpaceMarines;
+import database.SpaceMarinesDao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 /**
@@ -16,14 +22,34 @@ public class Clear extends Command {
      * @param collection collection
      * @return String description of command
      */
-    public String action(TreeMap<Integer, SpaceMarine> collection){
-        String message;
-        if (collection.isEmpty()){
-            message = "Collection is clear.";
-        } else {
-            collection.clear();
-            message = "Collection successfully cleared.";
+    public String action(TreeMap<Integer, SpaceMarine> collection, String login) {
+        boolean wasDeleted = false;
+        String message = "q";
+        SpaceMarinesDao spaceMarinesDao = new SpaceMarinesDao();
+        TreeMap<Integer, SpaceMarine> userCollection = null;
+        List<SpaceMarines> collect = new ArrayList<SpaceMarines>(spaceMarinesDao.findAll()) {
+        };
+        if (collect != null && !collect.isEmpty()) {
+            for (SpaceMarines spaceMarine : collect) {
+                if (spaceMarine.getUser().equals(login)) {
+                    spaceMarinesDao.delete(spaceMarine);
+                    wasDeleted = true;
+                }
+            }
         }
+        List<Integer> keys = new ArrayList<>();
+        for (Map.Entry<Integer, SpaceMarine> spaceMarineEntry : collection.entrySet()) {
+            if (spaceMarineEntry.getValue().getUser().equals(login)) {
+                System.out.println(spaceMarineEntry.getKey());
+                keys.add(spaceMarineEntry.getKey());
+            }
+        }
+        for (Integer delete : keys) {
+            collection.remove(delete);
+        }
+        if (!wasDeleted) message = "Your collection is empty.";
+        else message = "Your collection successfully cleared.";
+
         return message;
     }
 }
