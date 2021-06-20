b470aa1a3cf053b40aec88aff962fd3685a61449
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-20 18:56:53 +0300

    Update UserDao.java

--- Lab7/java/database/UserDao.java
+++ Lab7/java/database/UserDao.java
@@ -4,15 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class UserDao {
-    public User findByLogin(String login) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        session.createSQLQuery("select from USER where login=" + login)
-                .addEntity(User.class)
-                .list()
-                .forEach(System.out::println);
-        User user = session.get(User.class, login);
-        return null;
-    }
 
     public void save(User user) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
