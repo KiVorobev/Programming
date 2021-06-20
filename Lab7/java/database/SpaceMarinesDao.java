99b33502379b3a01d3f8bd954a5b76387a4315bd
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-20 18:56:48 +0300

    Update SpaceMarinesDao.java

--- Lab7/java/database/SpaceMarinesDao.java
+++ Lab7/java/database/SpaceMarinesDao.java
@@ -22,32 +22,19 @@ public class SpaceMarinesDao {
         return list;
     }
 
-    public void save(User user) {
+    public void save(SpaceMarines spaceMarines) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         Transaction tx1 = session.beginTransaction();
-        session.save(user);
+        session.save(spaceMarines);
         tx1.commit();
         session.close();
     }
 
-    public void update(User user) {
+    public void delete(SpaceMarines spaceMarines) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         Transaction tx1 = session.beginTransaction();
-        session.update(user);
+        session.delete(spaceMarines);
         tx1.commit();
         session.close();
-    }
-
-    public void delete(User user) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction tx1 = session.beginTransaction();
-        session.delete(user);
-        tx1.commit();
-        session.close();
-    }
-
-    public SpaceMarines findSpaceMarineById(int id) {
-        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(SpaceMarines.class, id);
-
     }
 }
