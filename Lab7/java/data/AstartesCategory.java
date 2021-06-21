9efa1f0ba1472c7dcb78af4bcb57d12a667eddfc
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-21 14:24:26 +0300

    Update AstartesCategory.java

--- Lab7/java/data/AstartesCategory.java
+++ Lab7/java/data/AstartesCategory.java
@@ -1,10 +1,13 @@
 package data;
 
+import javax.persistence.Embeddable;
+
 /**
  * @author Kirill Vorobyev
  * @version 1.0
  * Enum class for describing category of element
  */
+@Embeddable
 public enum AstartesCategory {
     ASSAULT,
     TACTICAL,
