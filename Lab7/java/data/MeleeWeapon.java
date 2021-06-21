d0f833ff8c7adbf4110ef96e7c87339d473a0ceb
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-21 14:24:36 +0300

    Update MeleeWeapon.java

--- Lab7/java/data/MeleeWeapon.java
+++ Lab7/java/data/MeleeWeapon.java
@@ -1,11 +1,13 @@
 package data;
 
+import javax.persistence.Embeddable;
+
 /**
  * @author Kirill Vorobyev
  * @version 1.0
  * Enum class for describing melee weapon of element
  */
-
+@Embeddable
 public enum MeleeWeapon {
     CHAIN_SWORD,
     CHAIN_AXE,
