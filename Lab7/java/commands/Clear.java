5bccb2da28fe3fa333a7a58940669513adb9ab0c
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-06-21 14:24:03 +0300

    Update Clear.java

--- Lab7/java/commands/Clear.java
+++ Lab7/java/commands/Clear.java
@@ -1,13 +1,10 @@
 package commands;
 
-import data.SpaceMarine;
 import database.SpaceMarines;
 import database.SpaceMarinesDao;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * Class of command 'clear'
@@ -19,34 +16,27 @@ public class Clear extends Command {
     /**
      * Method for executing this command
      *
-     * @param collection collection
      * @return String description of command
      */
-    public String action(TreeMap<Integer, SpaceMarine> collection, String login) {
+    public String action(String login) {
         boolean wasDeleted = false;
-        String message = "q";
+        String message;
         SpaceMarinesDao spaceMarinesDao = new SpaceMarinesDao();
-        TreeMap<Integer, SpaceMarine> userCollection = null;
         List<SpaceMarines> collect = new ArrayList<SpaceMarines>(spaceMarinesDao.findAll()) {
         };
         if (collect != null && !collect.isEmpty()) {
             for (SpaceMarines spaceMarine : collect) {
                 if (spaceMarine.getUser().equals(login)) {
-                    spaceMarinesDao.delete(spaceMarine);
+                    try {
+                        spaceMarinesDao.delete(spaceMarine);
+                    } catch (Exception exception) {
+                        return "Error occurred while removing an element.";
+                    }
                     wasDeleted = true;
                 }
             }
         }
-        List<Integer> keys = new ArrayList<>();
-        for (Map.Entry<Integer, SpaceMarine> spaceMarineEntry : collection.entrySet()) {
-            if (spaceMarineEntry.getValue().getUser().equals(login)) {
-                System.out.println(spaceMarineEntry.getKey());
-                keys.add(spaceMarineEntry.getKey());
-            }
-        }
-        for (Integer delete : keys) {
-            collection.remove(delete);
-        }
+
         if (!wasDeleted) message = "Your collection is empty.";
         else message = "Your collection successfully cleared.";
 
